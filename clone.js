51f41d60fa0bb14c044cf81b2e3a51d61c0eabab
CottnCor <gaotaomo@live.com>
2019-10-22 12:23:18 +0800

    :sparkles: :sparkles: :sparkles: add deepClone

---
+++ clone.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * @param {*} object
+ */
+const deepClone = (object) =>{
+  if(!object && typeof object !== 'object') return;
+  var tempObject = Array.isArray(object) ? new Array() : Object.create(null);
+  for (const key in object) {
+    if (typeof object[key] === 'object') {
+      tempObject[key] = deepClone(object[key]);
+    } else if(Array.isArray(object[key])) {
+      tempObject[key] = object[key];
+    }
+  }
+  return tempObject;
+}
+
+/**
+ *
+ * @param {*} object
+ */
+const shallowClone = (object) =>{
+
+}
