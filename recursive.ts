01669b3889d2a0b13ecbd2bedaaac1be98b616f3
CottnCor <gaotaomo@live.com>
2019-10-23 12:46:48 +0800

    :sparkles: :sparkles: :sparkles: add recursive

---
+++ recursive.ts
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+interface IFibonacci {
+  (): Function
+}
+
+/**
+ *
+ * @param {*} n
+ */
+const fibonacci: IFibonacci = () => {
+  const memory = [0, 1]
+  const fn = (n: number) => {
+    if (memory[n] != null) return memory[n]
+    return (memory[n] = fn(n - 2) + fn(n - 1))
+  }
+  return fn
+}
+
+console.log(fibonacci()(3))
---
+++ sort.ts
@@ -0,0 +1,116 @@
+/**
+ *
+ */
+const Compare: { LESS_THAN: number; BIGGER_THAN: number } = {
+  LESS_THAN: 0,
+  BIGGER_THAN: 1
+}
+
+/**
+ *
+ */
+interface ICompare<T> {
+  (a: T, b: T): number
+}
+
+/**
+ *
+ */
+interface ISwap<T> {
+  (array: Array<T>, a: number, b: number): void
+}
+
+/**
+ *
+ */
+interface ISort<T> {
+  (array: Array<T>, compareFn?: ICompare<T>): void
+}
+
+/**
+ *
+ * @param a
+ * @param b
+ */
+const defaultCompare: ICompare<any> = <T>(a: T, b: T) => {
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
+}
+
+/**
+ *
+ * @param {*} array
+ * @param {*} a
+ * @param {*} b
+ */
+const swap: ISwap<any> = <T>(array: Array<T>, a: number, b: number) => {
+  ;[array[a], array[b]] = [array[b], array[a]]
+}
+
+/**
+ *
+ * @param array
+ * @param compareFn
+ */
+const bubbleSort: ISort<any> = <T>(
+  array: Array<T>,
+  compareFn: ICompare<T> = defaultCompare
+) => {
+  const { length } = array
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - i - 1; j++) {
+      if (compareFn(array[j], array[j + 1]) === Compare.LESS_THAN) {
+        swap(array, j, j + 1)
+      }
+    }
+  }
+}
+
+/**
+ *
+ * @param array
+ * @param compareFn
+ */
+const selectionSort: ISort<any> = <T>(
+  array: Array<T>,
+  compareFn: ICompare<T> = defaultCompare
+) => {}
+
+/**
+ *
+ * @param array
+ * @param compareFn
+ */
+const insertionSort: ISort<any> = <T>(
+  array: Array<T>,
+  compareFn: ICompare<T> = defaultCompare
+) => {}
+
+/**
+ *
+ * @param array
+ * @param compareFn
+ */
+const mergeSort: ISort<any> = <T>(
+  array: Array<T>,
+  compareFn: ICompare<T> = defaultCompare
+) => {}
+
+/**
+ *
+ * @param array
+ * @param compareFn
+ */
+const quickSort: ISort<any> = <T>(
+  array: Array<T>,
+  compareFn: ICompare<T> = defaultCompare
+) => {}
+
+/**
+ *
+ * @param array
+ * @param compareFn
+ */
+const bucketSort: ISort<any> = <T>(
+  array: Array<T>,
+  compareFn: ICompare<T> = defaultCompare
+) => {}
