8e9c1fcb352b0c95df7271a62f6ebbf1c9635b91
CottnCor <gaotaomo@live.com>
2019-10-24 23:59:24 +0800

    :sparkles: :sparkles: :sparkles: update sort

--- sort.ts
+++ sort.ts
@@ -97,7 +97,19 @@ const selectionSort: ISort<any> = <T>(
 const insertionSort: ISort<any> = <T>(
   array: Array<T>,
   compareFn: ICompare<T> = defaultCompare
-) => {}
+) => {
+  const { length } = array
+  let temp
+  for (let i = 1; i < length; i++) {
+    let j = i
+    temp = array[i]
+    while (j > 0 && compareFn(array[j - 1], temp) === Compare.LESS_THAN) {
+      array[j] = array[j - 1]
+      j--
+    }
+    array[j] = temp
+  }
+}
 
 /**
  *
@@ -128,4 +140,3 @@ const bucketSort: ISort<any> = <T>(
   array: Array<T>,
   compareFn: ICompare<T> = defaultCompare
 ) => {}
-
